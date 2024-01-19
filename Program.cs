932af064d96ce80d4d86af9c779e2a684f582de7
NguyenAnhDuc42 <144120505+NguyenAnhDuc42@users.noreply.github.com>
2024-01-19 08:59:20 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5._1_2_3_4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList danhsach = new ArrayList();
+            Console.WriteLine("Hay Nhap So San Pham");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Hay Nhap Thong Tin Cho San Pham {i+1}");
+
+                Console.Write("Hay Nhap Ma SP:");
+                int  masp = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Hay Nhap Ten SP:");
+                string tensp = Convert.ToString(Console.ReadLine());
+                Console.Write("Hay NHap Don Gia:");
+                var dongia = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Hay Nhap So Luong:");
+                int soluong = Convert.ToInt32(Console.ReadLine());
+
+                SanPham sanPham = new SanPham(masp,tensp,dongia,soluong);
+                danhsach.Add(sanPham);
+            }
+            Console.WriteLine("Thong Tin SP");
+            foreach (SanPham sanPham in danhsach) 
+            {
+                sanPham.tinhtien();
+                sanPham.inra(); 
+            }
+            Console.ReadKey ();
+        }
+    }
+}
---
+++ SanPham.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5._1_2_3_4_5
+{
+    internal class SanPham
+    {
+        public int MaSP { get; set; }
+        public string TenSp { get; set; }
+        public float  DonGia { get; set; }
+        public int SoLuong { get; set; }
+        public float ThanhTien { get; set; }
+        public SanPham()
+        {
+        }
+        public SanPham(int masp, string tensp, float dongia, int soluong)
+        {
+            this.MaSP = masp;
+            this.TenSp = tensp;
+            this.DonGia = dongia;
+            this.SoLuong = soluong;
+        }
+        public void tinhtien()
+        {
+            this.ThanhTien = DonGia * SoLuong;
+        }
+        public void inra()
+        {
+            Console.WriteLine($"MaSP:{MaSP}\nTenSP:{TenSp}\nDonGia:{DonGia}\nSoLuong:{SoLuong}\nThanh Tien:{ThanhTien}");
+        }
+    }
+}
